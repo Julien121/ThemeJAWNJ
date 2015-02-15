9c67a906e98c6dcfe0896fb20218925c7bbf7b28
Julien <julien121.clement@gmail.com>
2015-02-15 20:21:30 +0100

    ThemeJWANJ

---
+++ assets/javascripts/main.js
@@ -0,0 +1,13 @@
+$(window).load(function(){
+	$('.slider').fractionSlider({
+		'fullWidth': 			true,
+		'controls': 			false, 
+		'pager': 				true,
+		'responsive': 			true,
+		'dimensions': 			"1140,400",
+	    'increase': 			false,
+		'pauseOnHover': 		true,
+		'slideEndAnimation': 	true
+	});
+
+});
